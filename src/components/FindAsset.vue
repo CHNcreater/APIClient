381d68058797615eaf4bd7bdb71f911e9ac2ae07
qiaoyinbo <breakerjoe@163.com>
2024-06-21 22:21:56 +0800

    Complete development

---
+++ src/components/FindAsset.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="grid-content ep-bg-purple">
+        <el-row>
+            <el-col :span="12">
+                <h1>{{ msg}}</h1>
+                <el-form :model="form">
+                    <el-form-item>
+                        <el-input v-model="form.url" placeholder="https://google.com"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-select v-model="form.option1" placeholder="Select">
+                            <el-option v-for="item in form.options" :key="item.value" :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-select v-model="form.dataFormat" placeholder="Select">
+                            <el-option v-for="item in form.dataFormats" :key="item.value" :label="item.label"
+                                :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input class="request-data" v-model="form.reqData" type="textarea"/>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" style="width: 100%;" @click="onClick">Send</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+            <el-col :span="12">
+                <h1>Response</h1>
+                <el-input v-model="response" class="response-data" type="textarea" placeholder='{"key": "value"}' style="padding: 20px;"></el-input>
+            </el-col>
+        </el-row>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref, defineProps } from 'vue'
+
+const props = defineProps({
+    msg: String
+})
+
+const form = reactive({
+    url: '',
+    option1: 'GET',
+    options: [
+        { value: 'GET', label: 'GET' },
+        { value: 'POST', label: 'POST' },
+        { value: 'PUT', label: 'PUT' },
+        { value: 'DELETE', label: 'DELETE' },
+        { value: 'PATCH', label: 'PATCH' },
+        { value: 'OPTIONS', label: 'OPTIONS' },
+        { value: 'HEAD', label: 'HEAD' },
+        { value: 'CONNECT', label: 'CONNECT' },
+        { value: 'TRACE', label: 'TRACE' },
+    ],
+    reqData: '',
+    dataFormat: 'JSON',
+    dataFormats: [
+        { value: 'JSON', label: 'JSON' },
+        { value: 'XML', label: 'XML' },
+        { value: 'HTML', label: 'HTML' },
+        { value: 'TEXT', label: 'TEXT' },
+    ],
+})
+
+const response = ref('')
+
+function onClick() {
+    //TODO Send http request
+    console.log('onClick')
+}
+</script>
+
+<style scoped>
+.el-form {
+    padding: 20px;
+}
+
+.grid-content {
+    height: calc(100vh - 160px);
+    overflow: auto;
+}
+
+:deep() .request-data .el-textarea__inner {
+    height: 150px;
+}
+
+:deep() .response-data .el-textarea__inner {
+    height: 300px;
+    background-color: #f5f7fa;
+}
+
+.el-input {
+    --el-input-border-color: #8b8b8b;
+}
+
+.el-select {
+    --el-border-color: #8b8b8b;
+}
+
+.el-textarea {
+    --el-border-color: #8b8b8b;
+}
+</style>
--- src/views/ApiTestView.vue
+++ src/views/ApiTestView.vue
@@ -1,43 +1,29 @@
 <template>
-  <el-tabs class="tabs" type="border-card">
-    <el-tab-pane label="Find Asset">
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-        hello<br>
-    </el-tab-pane>
-    <el-tab-pane label="Is Backed Up">Is Backed Up</el-tab-pane>
-    <el-tab-pane label="Last Backup">Last Backup</el-tab-pane>
-    <el-tab-pane label="Last Backup Size">Last Backup Size</el-tab-pane>
-    <el-tab-pane label="Next Backup Time">Next Backup Time</el-tab-pane>
-    <el-tab-pane label="Backup Status">Backup Status</el-tab-pane>
-  </el-tabs>
+    <el-tabs class="tabs" type="border-card">
+        <el-tab-pane label="Find Asset">
+            <FindAsset msg="Find Asset"/>
+        </el-tab-pane>
+        <el-tab-pane label="Is Backed Up">
+            <FindAsset msg="Is Backed Up"/>
+        </el-tab-pane>
+        <el-tab-pane label="Last Backup">
+            <FindAsset msg="Last Backup"/>
+        </el-tab-pane>
+        <el-tab-pane label="Last Backup Size">
+            <FindAsset msg="Last Backup Size"/>
+        </el-tab-pane>
+        <el-tab-pane label="Next Backup Time">
+            <FindAsset msg="Next Backup Time"/>
+        </el-tab-pane>
+        <el-tab-pane label="Backup Status">
+            <FindAsset msg="Backup Status"/>
+        </el-tab-pane>
+    </el-tabs>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import FindAsset from '../components/FindAsset.vue'
 
-<style scoped>
-.el-tab-pane {
-    height: calc(100vh - 130px);
-    overflow: auto;
-}
+</script>
 
-</style>
+<style scoped></style>
